4e9d5384fb5a656e2154151c48cf261635f3fe62
Bhoot Biswas <bhoot.biswas@gmail.com>
2017-07-04 21:43:04 +0600

    Add bootstrap, tether and popper

--- functions.php
+++ functions.php
@@ -148,6 +148,12 @@ function retouch_lite_scripts()
 
     wp_enqueue_script('retouch-lite-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true);
 
+    wp_enqueue_script('tether', get_template_directory_uri() . '/js/tether.min.js', array('jquery'), '20151215', true);
+
+    wp_enqueue_script('popper', get_template_directory_uri() . '/js/popper.min.js', array('jquery'), '20151215', true);
+
+    wp_enqueue_script('bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('tether'), '20151215', true);
+
     if (is_singular() && comments_open() && get_option('thread_comments')) {
         wp_enqueue_script('comment-reply');
     }
