08570fcd63b0bac2607ac5bde21ec7e8c31d5d19
Bhoot Biswas <bhoot.biswas@gmail.com>
2017-07-05 18:57:31 +0600

    Add basic css

--- template-parts/footer/footer-widgets.php
+++ template-parts/footer/footer-widgets.php
@@ -19,39 +19,43 @@ if (
 ?>
 
 	<aside class="widget-area" role="complementary">
-		<?php
-        if (is_active_sidebar('sidebar-2')) {
-            ?>
-			<div class="widget-column footer-widget-1">
-				<?php dynamic_sidebar('sidebar-2'); ?>
-			</div>
-		<?php
+		<div class="container">
+            <div class="wrap">
+                <?php
+                if (is_active_sidebar('sidebar-2')) {
+                    ?>
+        			<div class="widget-column footer-widget-1">
+        				<?php dynamic_sidebar('sidebar-2'); ?>
+        			</div>
+        		<?php
 
-        }
-        if (is_active_sidebar('sidebar-3')) {
-            ?>
-			<div class="widget-column footer-widget-2">
-				<?php dynamic_sidebar('sidebar-3'); ?>
-			</div>
-		<?php
+                }
+                if (is_active_sidebar('sidebar-3')) {
+                    ?>
+        			<div class="widget-column footer-widget-2">
+        				<?php dynamic_sidebar('sidebar-3'); ?>
+        			</div>
+        		<?php
 
-        }
-        if (is_active_sidebar('sidebar-4')) {
-            ?>
-			<div class="widget-column footer-widget-3">
-				<?php dynamic_sidebar('sidebar-4'); ?>
-			</div>
-		<?php
+                }
+                if (is_active_sidebar('sidebar-4')) {
+                    ?>
+        			<div class="widget-column footer-widget-3">
+        				<?php dynamic_sidebar('sidebar-4'); ?>
+        			</div>
+        		<?php
 
-        }
-        if (is_active_sidebar('sidebar-5')) {
-            ?>
-			<div class="widget-column footer-widget-4">
-				<?php dynamic_sidebar('sidebar-5'); ?>
-			</div>
-		<?php
+                }
+                if (is_active_sidebar('sidebar-5')) {
+                    ?>
+        			<div class="widget-column footer-widget-4">
+        				<?php dynamic_sidebar('sidebar-5'); ?>
+        			</div>
+        		<?php
 
-        } ?>
+                } ?>
+            </div>
+        </div>
 	</aside><!-- .widget-area -->
 
 <?php endif; ?>
